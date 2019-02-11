35ec12b4b4799575cca2d2aefb0278bfe201b139
Peter Hencz <peter.hencz@gmail.com>
2019-02-12 00:57:31 +0100

    nemes soon

--- nemes/src/App.js
+++ nemes/src/App.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import image from "./assets/nm.jpg";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      image: "src/assets/nm.jpg",
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <p>NEMES MARTON | soon.</p>
+          <img alt="img" src={image} />
+        </div>
       </div>
     );
   }
